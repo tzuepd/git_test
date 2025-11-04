2e5a867356bff0d1f7c329b8e492ab29793df7be
Tani <tze203@nyu.edu>
2025-11-04 17:00:51 -0500

    LED set up

--- git_test.ino
+++ git_test.ino
@@ -1,9 +1,8 @@
+int ledPin = 13;
+
 void setup() {
-  // put your setup code here, to run once:
-
+  pinMode(ledPin, OUTPUT);
 }
-
-//comment test
 
 void loop() {
   // put your main code here, to run repeatedly:
